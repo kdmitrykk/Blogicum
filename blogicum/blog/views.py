8a30ee3aa5a48a3e8806f16b936f28c59c5804a5
Dmitry Krikunov <TDimka1313@yandex.ru>
2024-12-10 15:21:33 +0300

    new_version

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -4,37 +4,54 @@ from django.shortcuts import get_object_or_404, render
 
 from blog.models import Category, Post
 
-post_list = Post.objects.select_related('category',
-                                        'author',
-                                        'location').filter(
-                                            is_published=True,
-                                            category__is_published=True,
-                                            pub_date__lte=datetime.
-                                            datetime.now(
-                                            ))
 
-category = Category.objects.values('title',
-                                   'description').filter(is_published=True)
+POSTS_PER_PAGE = 5
 
 
 def index(request):
     """Главная страница проекта."""
-    post_list_limit = post_list[0:5]
-    context = {'post_list': post_list_limit}
+    post_list = Post.objects.select_related('category',
+                                            'author',
+                                            'location').filter(
+                                                is_published=True,
+                                                category__is_published=True,
+                                                pub_date__lte=datetime.
+                                                datetime.now(
+                                                ))
+    post_list = post_list[:POSTS_PER_PAGE]
+    context = {'post_list': post_list}
     return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, post_id):
     """Страница отдельного блога."""
-    post = get_object_or_404(post_list.filter(pk=post_id))
+    post_list = Post.objects.select_related('category',
+                                            'author',
+                                            'location').filter(
+                                                is_published=True,
+                                                category__is_published=True,
+                                                pub_date__lte=datetime.
+                                                datetime.now(
+                                                ))
+    post = get_object_or_404(post_list, pk=post_id)
     context = {'post': post}
     return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug):
     """Страница отдельной категории."""
-    this_category = get_object_or_404(category, slug=category_slug)
-    post_list_category = post_list.filter(category__slug=category_slug)
-    context = {'post_list': post_list_category,
-               'category': this_category}
+    category = get_object_or_404(Category.objects.values('title',
+                                                         'description'),
+                                 slug=category_slug,
+                                 is_published=True)
+    post_list = Post.objects.select_related('category',
+                                            'author',
+                                            'location').filter(
+                                                is_published=True,
+                                                category__is_published=True,
+                                                pub_date__lte=datetime.
+                                                datetime.now(),
+                                                category__slug=category_slug)
+    context = {'post_list': post_list,
+               'category': category}
     return render(request, 'blog/category.html', context)
